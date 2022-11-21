eb6be98d1ab2cc52dd79203416bf226d4116bd2c
emilioo2 <112031914+emilioo2@users.noreply.github.com>
2022-11-21 17:37:06 -0500

    Add files via upload
    now with flexboxes

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// function myfunction0(){
+//     alert('What Up?')
+// }
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
